fb250f06fbaf70c443211053a26a99610a7a5dff
ske <sixtisam@gmail.com>
2020-03-23 10:57:06 +0100

    week 6

--- week6/infection/infection.js
+++ week6/infection/infection.js
@@ -1,26 +1,58 @@
-
-const canvas  = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-context.fillStyle = "black";
+const Person = x => y => z => f => f(x)(y)(z);
+const x = x => y => z => x;
+const y = x => y => z => y;
+const infected = x => y => z => z;
 
-const radius = 10;
+let persons = [Person(100)(100)(false)];
 
-const ball = {x: 100, y: 100} ;
+const createPerson = infected => () => {
+  persons.push(Person(5)(5)(infected));
+};
+
+document.getElementById("person-add").onclick = createPerson(false);
+document.getElementById("infected-person-add").onclick = createPerson(true);
+
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = 800;
+const RADIUS = 10;
 
 function start() {
-    setInterval(() => {
-        nextBoard();
-    }, 1000 / 50);
+  setInterval(() => {
+    calcNextBoard();
+  }, 1000 / 50);
+
+  requestAnimationFrame(drawBoard);
 }
 
-function nextBoard() {
-    fillBox(ball, radius);
+function calcNextBoard() {
+  persons = persons.map(p => {
+    const dx = Math.ceil(Math.random() * 10);
+    const dy = Math.ceil(Math.random() * 10);
+    return Person((p(x) + dx) % CANVAS_WIDTH)((p(y) + dy) % CANVAS_HEIGHT)(
+      touching
+    );
+  });
 }
 
-function fillBox(ball, radius) {
-    context.beginPath();
-    context.arc(ball.x, ball.y, radius, 0, 6.3, false);
-    context.fill();
+/**
+ * Redraws the complete board
+ */
+function drawBoard() {
+  context.fillStyle = "gold";
+  context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  persons.forEach(drawPerson);
+  requestAnimationFrame(drawBoard);
 }
 
-
+function drawPerson(person) {
+  if (person(infected)) {
+    context.fillStyle = "red";
+  } else {
+    context.fillStyle = "black";
+  }
+  context.beginPath();
+  context.arc(person(x), person(y), RADIUS, 0, 6.3, false);
+  context.fill();
+}
--- week6/oopsie/oopsie.js
+++ week6/oopsie/oopsie.js
@@ -8,45 +8,69 @@
 //   proceed(stride) // proceed so many places
 //   fallback()      // "oopsie": go back to last start (fallback position)
 //   turn()          // cash in your win, update fallback position for next turn
-// 
+//
+
+const Player = function Player(name) {
+  let _name = name;
+  let _fallbackIndex = 0;
+  let _progressIndex = 0;
+
+  return {
+    proceed: stride => {
+      _progressIndex += stride;
+    },
+    fallback: () => {
+      _progressIndex = _fallbackIndex;
+    },
+    turn: () => {
+      _fallbackIndex = _progressIndex;
+    },
+    getFallbackIndex: () => _fallbackIndex,
+    getProgressIndex: () => _progressIndex
+  };
+};
 
 function start() {
-    const fields = document.getElementById('fields');
+  const fields = document.getElementById("fields");
 
-    for (let i = 0; i < 100; i++) {
-        let field = document.createElement("DIV");
-        field.setAttribute("ID", "FIELD-"+i);
-        field.innerText = " ";
-        fields.appendChild(field);
-    }
-    display();
+  for (let i = 0; i < 100; i++) {
+    let field = document.createElement("DIV");
+    field.setAttribute("ID", "FIELD-" + i);
+    field.innerText = " ";
+    fields.appendChild(field);
+  }
+  display();
 }
 
 function dice() {
-    let stride = Math.round(1 + Math.random() * 5);
-    document.getElementById('dice').innerText = ""+ stride;
-    if (stride === 3) {
-        player.fallback();
-    } else {
-        player.proceed(stride);
-    }
-    display();
+  let stride = Math.round(1 + Math.random() * 5);
+  document.getElementById("dice").innerText = "" + stride;
+  if (stride === 3) {
+    player.fallback();
+  } else {
+    player.proceed(stride);
+  }
+  display();
 }
 
 function turn() {
-    player.turn();
-    display();
+  player.turn();
+  display();
 }
 
 function display() {
-    for (let i = 0; i < 100; i++) {
-        let field = document.getElementById("FIELD-"+i);
-        field.setAttribute("CLASS", "field");
-    }
-    let fallbackfield = document.getElementById("FIELD-"+ player.getFallbackIndex());
-    fallbackfield.setAttribute("CLASS", "field fallback");
-    let progressfield = document.getElementById("FIELD-"+ player.getProgressIndex());
-    progressfield.setAttribute("CLASS", "field progress");
+  for (let i = 0; i < 100; i++) {
+    let field = document.getElementById("FIELD-" + i);
+    field.setAttribute("CLASS", "field");
+  }
+  let fallbackfield = document.getElementById(
+    "FIELD-" + player.getFallbackIndex()
+  );
+  fallbackfield.setAttribute("CLASS", "field fallback");
+  let progressfield = document.getElementById(
+    "FIELD-" + player.getProgressIndex()
+  );
+  progressfield.setAttribute("CLASS", "field progress");
 }
 
 player = Player("One");
