8329b1ef253f964c9848f6495d03dbc4a9f90ddc
ske <sixtisam@gmail.com>
2020-05-18 09:48:48 +0200

    week 11

--- week12/todo/todo.js
+++ week12/todo/todo.js
@@ -1,124 +1,117 @@
 // requires ../observable/observable.js
 
 const TodoController = () => {
-
-    const Todo = () => {                                // facade
-        const textAttr = Observable("...");            // we current don't expose it as we don't use it elsewhere
-        const doneAttr = Observable(false);
-        return {
-            getDone:       doneAttr.getValue,
-            setDone:       doneAttr.setValue,
-            onDoneChanged: doneAttr.onChange,
-            setText:       textAttr.setValue,
-            getText:       textAttr.getValue,
-            onTextChanged: textAttr.onChange,
-        }
-    };
-
-    const todoModel = ObservableList([]); // observable array of Todos, this state is private
-
-    const addTodo = () => {
-        const newTodo = Todo();
-        todoModel.add(newTodo);
-        return newTodo;
-    };
-//    const scheduler = Scheduler();
-    const addFortuneTodo = button => {
-//        button.disabled = true;
-        const newTodo = Todo();
-        todoModel.add(newTodo);
-        newTodo.setText("...");
-
-//        scheduler.add( ok => {
-            fortuneService( text => {
-                newTodo.setText(text);
-//                button.disabled = false;
-                ok();
-            });
-//        });
-
-
-        return newTodo;
-    };
-
+  const Todo = () => {
+    // facade
+    const textAttr = Observable("..."); // we current don't expose it as we don't use it elsewhere
+    const doneAttr = Observable(false);
     return {
-        numberOfTodos:      todoModel.count,
-        numberOfopenTasks:  () => todoModel.countIf( todo => ! todo.getDone() ),
-        addTodo:            addTodo,
-        addFortuneTodo:     addFortuneTodo,
-        removeTodo:         todoModel.del,
-        onTodoAdd:          todoModel.onAdd,
-        onTodoRemove:       todoModel.onDel,
-        removeTodoRemoveListener: todoModel.removeDeleteListener, // only for the test case, not used below
-    }
-};
+      getDone: doneAttr.getValue,
+      setDone: doneAttr.setValue,
+      onDoneChanged: doneAttr.onChange,
+      setText: textAttr.setValue,
+      getText: textAttr.getValue,
+      onTextChanged: textAttr.onChange,
+    };
+  };
 
+  const todoModel = ObservableList([]); // observable array of Todos, this state is private
+
+  const addTodo = () => {
+    const newTodo = Todo();
+    todoModel.add(newTodo);
+    return newTodo;
+  };
+  const scheduler = Scheduler();
+  const addFortuneTodo = (button) => {
+    //        button.disabled = true;
+    const newTodo = Todo();
+    todoModel.add(newTodo);
+    newTodo.setText("...");
+
+    scheduler.add((ok) => {
+      fortuneService((text) => {
+        newTodo.setText(text);
+        //                button.disabled = false;
+        ok();
+      });
+    });
+
+    return newTodo;
+  };
+
+  return {
+    numberOfTodos: todoModel.count,
+    numberOfopenTasks: () => todoModel.countIf((todo) => !todo.getDone()),
+    addTodo: addTodo,
+    addFortuneTodo: addFortuneTodo,
+    removeTodo: todoModel.del,
+    onTodoAdd: todoModel.onAdd,
+    onTodoRemove: todoModel.onDel,
+    removeTodoRemoveListener: todoModel.removeDeleteListener, // only for the test case, not used below
+  };
+};
 
 // View-specific parts
 
 const TodoItemsView = (todoController, rootElement) => {
-
-    const render = todo => {
-
-        function createElements() {
-            const template = document.createElement('DIV'); // only for parsing
-            template.innerHTML = `
+  const render = (todo) => {
+    function createElements() {
+      const template = document.createElement("DIV"); // only for parsing
+      template.innerHTML = `
                 <button class="delete">&times;</button>
                 <input type="text" size="36">
                 <input type="checkbox">            
             `;
-            return template.children;
-        }
-        const [deleteButton, inputElement, checkboxElement] = createElements();
+      return template.children;
+    }
+    const [deleteButton, inputElement, checkboxElement] = createElements();
 
-        checkboxElement.onclick = _ => todo.setDone(checkboxElement.checked);
-        deleteButton.onclick    = _ => todoController.removeTodo(todo);
+    checkboxElement.onclick = (_) => todo.setDone(checkboxElement.checked);
+    deleteButton.onclick = (_) => todoController.removeTodo(todo);
 
-        todoController.onTodoRemove( (removedTodo, removeMe) => {
-            if (removedTodo !== todo) return;
-            rootElement.removeChild(inputElement);
-            rootElement.removeChild(deleteButton);
-            rootElement.removeChild(checkboxElement);
-            removeMe();
-        } );
+    todoController.onTodoRemove((removedTodo, removeMe) => {
+      if (removedTodo !== todo) return;
+      rootElement.removeChild(inputElement);
+      rootElement.removeChild(deleteButton);
+      rootElement.removeChild(checkboxElement);
+      removeMe();
+    });
 
-        todo.onTextChanged( _ => inputElement.value = todo.getText() );
+    todo.onTextChanged((_) => (inputElement.value = todo.getText()));
 
-        rootElement.appendChild(deleteButton);
-        rootElement.appendChild(inputElement);
-        rootElement.appendChild(checkboxElement);
-    };
+    rootElement.appendChild(deleteButton);
+    rootElement.appendChild(inputElement);
+    rootElement.appendChild(checkboxElement);
+  };
 
-    // binding
+  // binding
 
-    todoController.onTodoAdd(render);
+  todoController.onTodoAdd(render);
 
-    // we do not expose anything as the view is totally passive.
+  // we do not expose anything as the view is totally passive.
 };
 
 const TodoTotalView = (todoController, numberOfTasksElement) => {
+  const render = () =>
+    (numberOfTasksElement.innerText = "" + todoController.numberOfTodos());
 
-    const render = () =>
-        numberOfTasksElement.innerText = "" + todoController.numberOfTodos();
+  // binding
 
-    // binding
-
-    todoController.onTodoAdd(render);
-    todoController.onTodoRemove(render);
+  todoController.onTodoAdd(render);
+  todoController.onTodoRemove(render);
 };
 
 const TodoOpenView = (todoController, numberOfOpenTasksElement) => {
+  const render = () =>
+    (numberOfOpenTasksElement.innerText =
+      "" + todoController.numberOfopenTasks());
 
-    const render = () =>
-        numberOfOpenTasksElement.innerText = "" + todoController.numberOfopenTasks();
+  // binding
 
-    // binding
-
-    todoController.onTodoAdd(todo => {
-        render();
-        todo.onDoneChanged(render);
-    });
-    todoController.onTodoRemove(render);
+  todoController.onTodoAdd((todo) => {
+    render();
+    todo.onDoneChanged(render);
+  });
+  todoController.onTodoRemove(render);
 };
-
-
